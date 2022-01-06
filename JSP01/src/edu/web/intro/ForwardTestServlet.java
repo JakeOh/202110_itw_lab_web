2b24bc1a0668c4707d2dfc466a5043c0e94e1334
Jake Oh <jake.oh.lecture@gmail.com>
2022-01-06 17:33:20 +0900

    .

--- JSP01/src/edu/web/intro/ForwardTestServlet.java
+++ JSP01/src/edu/web/intro/ForwardTestServlet.java
@@ -34,6 +34,13 @@ public class ForwardTestServlet extends HttpServlet {
 		// 전달받은 요청(request)을 JSP 페이지에게 재전송(forward)
 		RequestDispatcher dispatcher = request.getRequestDispatcher("forward.jsp");
 		dispatcher.forward(request, response);
+		
+		// forward 방식의 페이지 이동:
+		// 클라이언트가 보낸 요청(request)을 유지하면서 새로운 페이지로 이동.
+		// request, response 객체가 유지됨.
+		// 같은 서버의 같은 웹 애플리케이션 안에서만 페이지 이동이 가능.
+		// 다른 서버, 다른 웹 애플리케이션으로 페이지 이동은 불가능.
+		// 페이지가 이동하더라도 요청 URL가 유지됨(변경되지 않음).
 	}
 
 	/**
