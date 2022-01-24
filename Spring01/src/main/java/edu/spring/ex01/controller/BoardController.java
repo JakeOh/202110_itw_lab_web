08eada57d935c6148c90b43ac9aebcc5cf622dd3
Jake Oh <jake.oh.lecture@gmail.com>
2022-01-24 11:27:53 +0900

    .

--- Spring01/src/main/java/edu/spring/ex01/controller/BoardController.java
+++ Spring01/src/main/java/edu/spring/ex01/controller/BoardController.java
@@ -1,10 +1,9 @@
 package edu.spring.ex01.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,11 +25,18 @@ public class BoardController {
 	}
 	
 	@RequestMapping(value = "/insert", method = RequestMethod.POST)
-	public String boardInsertPost(int bno, String title) {
-		//int bno = Integer.parseInt(req.getParameter("bno"));
-		//String title = req.getParameter("title");
+	public String boardInsertPost(int bno, String title, Model model) {
+		// int bno = Integer.parseInt(req.getParameter("bno"));
+		// String title = req.getParameter("title");
+		// Controlloer 메서드에서 파라미터 이름을 요청 파라미터(form 요소들의 name 속성) 이름과 동일하게 작성하면
+		// Dispatcher Servlet이 요청 파라미터들을 분석해서(request.getParameter() 호출해서)
+		// 메서드를 호출할 때 argument로 전달해줌.
 		
 		logger.info("boardInsertPost() 호출: bno = {}, title = {}", bno, title);
+		
+		// JSP에게 데이터를 전달할 때, Model 객체에 속성(attr)을 추가
+		model.addAttribute("bno", bno);
+		model.addAttribute("title", title);
 		
 		return "board/insert-result"; // WEB-INF/views/board/insert-result.jsp
 	}
