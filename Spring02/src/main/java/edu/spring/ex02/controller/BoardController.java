c843af8364db6ddd4dc7630922ed4f34f14752b9
Jake Oh <jake.oh.lecture@gmail.com>
2022-01-26 12:24:15 +0900

    .

---
+++ Spring02/src/main/java/edu/spring/ex02/controller/BoardController.java
@@ -0,0 +1,19 @@
+package edu.spring.ex02.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/board")
+public class BoardController {
+	private static final Logger log = LoggerFactory.getLogger(BoardController.class);
+	
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public void main() {
+		log.info("main() 호출");
+	}
+	
+}
